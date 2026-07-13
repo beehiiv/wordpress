0fdf379d9b52b2daef10fc96ba7c9a9dcf4aa81d
Zack <zack@kairaweb.com>
2026-07-13 12:17:18 +0200

    tighten REST permission checks as recommended in plugin review

--- includes/REST/AdvertisementOpportunitiesController.php
+++ includes/REST/AdvertisementOpportunitiesController.php
@@ -69,7 +69,7 @@ final class AdvertisementOpportunitiesController {
 	/**
 	 * Permission check for advertisement requests.
 	 *
-	 * Requires edit access to the referenced post and publish_posts (newsletter/ad UI).
+	 * Requires edit access to the referenced post (Contributors included for drafts).
 	 *
 	 * @since 1.0.0
 	 *
@@ -80,11 +80,7 @@ final class AdvertisementOpportunitiesController {
 
 		$post_id = absint( $request->get_param( 'post_id' ) );
 
-		if ( ! $post_id ) {
-			return false;
-		}
-
-		return current_user_can( 'edit_post', $post_id ) && current_user_can( 'publish_posts' );
+		return $post_id && current_user_can( 'edit_post', $post_id );
 	}
 
 	/**
